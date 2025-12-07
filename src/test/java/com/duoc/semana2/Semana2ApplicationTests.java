ceda6fbd46cc2817bcd78cb2ff243bb21319e7b9
jai-valencia <jai.valencia@duocuc.cl>
2025-12-07 00:24:22 -0300

    cambios

--- src/test/java/com/duoc/semana2/Semana2ApplicationTests.java
+++ src/test/java/com/duoc/semana2/Semana2ApplicationTests.java
@@ -1,14 +1,13 @@
 package com.duoc.semana2;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class Semana2ApplicationTests {
+@Disabled("Clase deshabilitada para evitar cargar el contexto")
+class Semana2ApplicationTests {
 
     @Test
-    @Disabled("Desactivado porque el contexto no carga y no es requerido")
     void contextLoads() {}
-
 }
